6de18d62e2b788273655817c23422521c2014fde
mykyta07 <mykyta@gmail.com>
2023-10-16 11:58:42 +0300

    Лаб. роб. виконана

---
+++ main.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const host = "localhost";
+const port = 8000;
+
+const xml = require("fast-xml-parser");
+const fs = require("node:fs");
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  function minvalue(xmldata) {
+    let min = 10000;
+    for (let list of xmldata.indicators.res) {
+      if (parseFloat(list.value) < min) {
+        min = parseFloat(list.value);
+      }
+    }
+    return min;
+  }
+
+  const data = fs.readFileSync("data.xml", "utf-8");
+  const parser = new xml.XMLParser();
+  const obj = parser.parse(data);
+  const mindata = minvalue(obj);
+
+  const builder = new xml.XMLBuilder();
+
+  const xmlres = builder.build({
+    result: {
+      value: mindata,
+    },
+  });
+
+  fs.writeFileSync("res.xml", xmlres);
+
+  res.end("Server is running!!!");
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
